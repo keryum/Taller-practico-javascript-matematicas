27a6e9b6b3da073064611f1fc7eb0670d9258582
keryum <alejandro_suarezp@outlook.com>
2023-05-02 21:03:47 -0500

    avance en clase descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,31 +1,73 @@
 // Precios y descuentos
 
-function PreciosYDescuentos(precio, descuento) {
-    const precioConDescuento = (precio * (100-descuento))/100;
-    return {
-        precioConDescuento
-    }
-}
+// function PreciosYDescuentos(precio, descuento) {
+//     const precioConDescuento = (precio * (100-descuento))/100;
+//     return {
+//         precioConDescuento
+//     }
+// }
+// const inputPrice = (document.querySelector('#price'))
+// const inputDiscount = (document.querySelector('#discount'))
+// const button = document.querySelector('#calcular')
+// const result = document.querySelector('#result')
+
+// button.addEventListener('click', showResult)
+// function showResult() {
+//     const price = inputPrice.value;
+//     const discount = inputDiscount.value;
+//     const newPrice = (price * (100-discount))/100;
+//     if (!price || !discount) {
+//         result.innerText = 'Por favor, llena los espacios solicitados'
+//     } else if (discount > 100) {
+//         result.innerText = 'El valor del descuento no puede ser mayor a 100'
+//     } else
+//     result.innerText = `El nuevo precio con descuento es $${newPrice}`;
+// }
+
+// Cupones
+
 const inputPrice = (document.querySelector('#price'))
-const inputDiscount = (document.querySelector('#discount'))
-const button = document.querySelector('#calcular')
+const inputCupon = (document.querySelector('#cupon'))
+const button = document.querySelector('#verificar-cupon')
 const result = document.querySelector('#result')
 
-button.addEventListener('click', showResult)
-function showResult() {
+button.addEventListener('click', canjearCupon)
+function canjearCupon() {
+    const cupon = inputCupon.value;
     const price = inputPrice.value;
-    const discount = inputDiscount.value;
-    const newPrice = (price * (100-discount))/100;
-    if (!price || !discount) {
-        result.innerText = 'Por favor, llena los espacios solicitados'
-    } else if (discount > 100) {
-        result.innerText = 'El valor del descuento no puede ser mayor a 100'
-    } else
-    result.innerText = `El nuevo precio con descuento es $${newPrice}`;
+    
+if (!price || !cupon) {
+    result.innerText = 'Por favor, llena los espacios solicitados';
+    return;
 }
-// function renderizarElementos (header) {
-//     const header = document.createElement('header')
-//     const text = document.createElement('p')
-//     header.appendChild(title)
-//     text.textContent = 'Hola'
-// }
+
+    let discount;
+
+    switch (cupon) {
+
+        case 'DesCuEnTiTo25': 
+        discount = 25;
+        break;
+
+        case 'DesCuEnTiTo50':
+        discount = 50;
+        break;
+
+        default:
+        result.innerText = `El cupón que ha ingresado es invalido`
+        return;
+        
+    }
+
+    // if (cupon === 'DesCuEnTiTo25') {
+    //     discount = 25
+    // } else if (cupon === 'DesCuEnTiTo50') {
+    //     discount = 50
+    // } else {
+    //     result.innerText = `El cupón que ha ingresado es invalido`;
+    //     return;
+    // }
+
+    const newPrice = (price * (100-discount))/100;
+    result.innerText = `EL nuevo precio con descuento es $${newPrice}`
+}
