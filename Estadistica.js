c03514bc0959a1d680896dbafec63e10d6f28184
keryum <alejandro_suarezp@outlook.com>
2023-05-04 17:08:43 -0500

    finalización de sección estadistica e inicio de salarios

--- Estadistica.js
+++ Estadistica.js
@@ -1,40 +1,66 @@
+// mediana
+
+NewMath = {}
+
+NewMath.esImpar = function esImpar(array) { 
+    return array.length%2;
+}
+NewMath.calcularMediana = function calcularMediana (array) {
+    array.sort((a,b) => a-b);
+    const arrayEsImpar = NewMath.esImpar(array);
+    
+    if (!arrayEsImpar) {
+        const ObjetoMedianaPar1 = array[(array.length/2)-1];
+        const ObjetoMedianaPar2 = array[array.length/2];
+        const ObjetosMediana = [ObjetoMedianaPar1, ObjetoMedianaPar2]
+        const medianaPar = NewMath.calcularPromedio (ObjetosMediana)
+        return medianaPar
+    } else {
+        const IndexMedianaImpar = (Math.floor(array.length/2))
+        const medianaImpar = array[IndexMedianaImpar]
+        return medianaImpar
+    }
+}
+
 // promedio
-
-const array = [5, 1, 4, 3, 8, 10]
-
-
-function calcularPromedio(array) {
-    
-    // let sumaArray = 0;
-
-    // for (let i = 0; i < array.length; i++) {
-    //     sumaArray = sumaArray + array[i];
-    // }
-    
-    // function sumaArray(valorAcumulado, nuevoValor) {
+NewMath.calcularPromedio = function calcularPromedio(array) {
+    // function sumarElementos(valorAcumulado, nuevoValor) {
     //     return valorAcumulado + nuevoValor;
     // }
 
     // const sumaTotal = array.reduce(sumaArray);
 
-    const sumaArray = array.reduce((a, b) => a + b);
-
+    const sumaArray = array.reduce((a,b) => a + b);
     const promedio = (sumaArray / array.length);
-    return promedio
+    return promedio;
 }
 
+// ordenar listas
+NewMath.ordenarLista = function ordenarLista (array) {
+    const arrayOrdenado = array.sort((a, b) => a - b);
+    return arrayOrdenado;
+}
 
-// mediana
+// Moda
 
+NewMath.calcularModa = function calcularModa(array) {
+    const arrayCount = {};
 
-const esImpar = (array) => array.length%2
-function calcularMediana (array) {
-    array.sort((a,b) => a-b);
-    const arrayEsImpar = esImpar(array);
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i]
 
-    if (arrayEsImpar) {
-        const IndiceMedianaImpar = (Math.round(array.length/2)-1)
-        return array[IndiceMedianaImpar]
-    } else { 
+        if (!arrayCount[element]) {
+            arrayCount[element] = 1;
+        } else {
+            arrayCount[element] += 1;
+        }
     }
+    
+    const arrayOfArrays = Object.entries(arrayCount)
+
+    arrayOfArrays.sort((a,b) => b[1]-a[1])
+    
+    const moda = arrayOfArrays[0][0]
+
+    return moda
 }
